cdca839f3591b3f36cfed14e66d039ba38384c56
Speshni0k-Maksym <popovichspgmaster1@gmail.com>
2026-02-27 19:04:22 +0200

    Update

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from fastapi import FastAPI
+import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
 API_TOKEN = os.getenv("BOT_TOKEN")
-    
+PORT = int(os.getenv("PORT", 8080))
+
 disp = Dispatcher()
+app = FastAPI()
 
 @disp.message(Command("start"))
 async def startMethod(message: Message):
     await message.answer("Bot active")
 
-
+@app.get("/")
+async def checker():
+    return {"status": "Bot is active"}
 
 
 
@@ -25,4 +31,6 @@ async def main():
     await disp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    result = asyncio.get_event_loop()
+    result.create_task(main())
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
