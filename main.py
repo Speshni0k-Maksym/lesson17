fca546d24dc556b645a1c65e4c17974d86843d3d
Speshni0k-Maksym <popovichspgmaster1@gmail.com>
2026-02-27 20:04:17 +0200

    1

--- main.py
+++ main.py
@@ -35,6 +35,11 @@ async def main():
 
 async def runner():
     asyncio.run(main())
+
+@app.on_event("startup")
+async def startup():
+    bot  = Bot(token=BOT_TOKEN)
+    asyncio.create_task(disp.start_polling(bot))
     
     
 if __name__ == "__main__":
