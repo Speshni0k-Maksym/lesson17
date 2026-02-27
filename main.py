7fd7a9e543a7ab043682a477d417ffb91306047a
Speshni0k-Maksym <popovichspgmaster1@gmail.com>
2026-02-27 19:40:39 +0200

    1

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
-API_TOKEN = os.getenv("BOT_TOKEN")
-PORT = int(os.getenv("PORT", 8080))
+#API_TOKEN = os.getenv("BOT_TOKEN")
+#PORT = int(os.getenv("PORT", 8080))
+BOT_TOKEN = "8571820554:AAFuvPpbdK4jewtTMvaWon4ScSn5r4A_fIE"
+PORT = 8080
 
 disp = Dispatcher()
 app = FastAPI()
@@ -26,11 +28,10 @@ async def checker():
 
 
 async def main():
-    bot = Bot(token=API_TOKEN)
-
+    bot = Bot(token=BOT_TOKEN)
     await disp.start_polling(bot)
 
 if __name__ == "__main__":
     result = asyncio.get_event_loop()
     result.create_task(main())
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run(app,host = "0.0.0.0", port=PORT)
